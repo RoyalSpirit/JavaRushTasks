e34ecf3cff9d07d9c2fe6c9111d75891aa5633b4
RoyalSpirit <mettour@yandex.ru>
2019-06-16 18:39:21 +0300

    Задача 3

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0111/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0111;
+
+/* 
+Одной переменной недостаточно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 34;
+        String s = "Единая Россия - партия жуликов и воров.";
+    }
+}
