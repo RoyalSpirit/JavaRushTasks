3e3fc88f9f9e741e132c43d627031084d8f58fc3
RoyalSpirit <mettour@yandex.ru>
2019-06-16 18:31:40 +0300

    Задача 2

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0110/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task01.task0110;
+
+/* 
+На нашем экране — переменная
+*/
+
+import java.sql.SQLOutput;
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Завтра мне снимут гипс, Ура !!!";
+        System.out.println(name);
+
+    }
+}
