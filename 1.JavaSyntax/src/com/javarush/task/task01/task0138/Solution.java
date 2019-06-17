da896b01e9d6a17dadfe2b8a97b24d716c56f31b
RoyalSpirit <mettour@yandex.ru>
2019-06-17 21:25:24 +0300

    Задача 19

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println("");
+        System.out.println("У лукоморья дуб зеленый");
+        System.out.println("Золотая цепь на дубе том");
+        System.out.println("И днем и ночью кот ученый");
+        System.out.println("Все ходит по цепи кругом");
+        System.out.println("Идет направо - песнь заводит");
+        System.out.println("Налево - сказку говорит");
+    }
+}
