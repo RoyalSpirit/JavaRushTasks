024b3b7fbce4c4dae31e0e7611e75b4216e2ccea
RoyalSpirit <mettour@yandex.ru>
2019-10-07 22:10:12 +0300

    Задача 47

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0309/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task03.task0309;
+
+/* 
+Сумма 5 чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int one = 1;
+        int sum = 1 + 2;
+        int sum1 = 1 + 2 + 3;
+        int sum2 = 1 + 2 + 3 + 4;
+        int sum3 = 1 + 2 + 3 + 4 + 5;
+
+        System.out.println(one);
+        System.out.println(sum);
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+    }
+}
