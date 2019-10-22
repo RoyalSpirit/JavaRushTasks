504f0aa748fa1a8214fc7565e8929772e58d726f
RoyalSpirit <mettour@yandex.ru>
2019-10-22 22:53:53 +0300

    Задача 80

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task04.task0415;
+
+/* 
+Правило треугольника
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+
+        if (a + b > c && b + c > a && a + c > b)
+            System.out.println("Треугольник существует.");
+        else if (a + b == c || b + c == a || a + c == b)
+        System.out.println("Треугольник не существует.");
+        else System.out.println("Треугольник не существует.");
+
+    }
+}
